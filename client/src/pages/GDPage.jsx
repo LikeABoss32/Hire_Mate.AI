fc9337f2a4957e6c080cb3f0a04d0a9a7f7392a6
LikeABoss32 <upretikaran7@gmail.com>
2026-05-07 00:50:04 +0530

    Update GDPage.jsx

--- client/src/pages/GDPage.jsx
+++ client/src/pages/GDPage.jsx
@@ -565,7 +565,7 @@ function GDPage() {
                     isMicOn ? 'bg-red-500 text-white' : 'bg-gray-700 text-gray-300 hover:bg-gray-600'
                   }`}>
 
-                   <FaMicrophone size={14} />
+                 
 
                   <FaMicrophone size={14} />
 
